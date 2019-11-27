98526eb56379639f76ccd0e73cfe50cbe63ba024
MiltonValenzuela <miltvalenz@hotmail.com>
2019-11-27 16:47:37 -0400

    Challenge 'Javascript Dates' completed

---
+++ Day6/Dates.js
@@ -0,0 +1,32 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    let newDate = new Date(dateString).getDay();
+
+    switch (newDate){
+     case 0:
+        dayName = "Sunday";
+        break;
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    }
+
+    return dayName;
+}
